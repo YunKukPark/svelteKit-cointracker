a34d69d5d19db0d1db245ceb731a5da700a496f8
YunKukPark <a01043340999@gmail.com>
2022-02-07 17:20:57 +0900

    Add: tailwind & Router 셋팅

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<svelte:head>
+  <title>코인</title>
+</svelte:head>
+<h1 class="text-3xl font-bold underline text-blue-300">
+  메인페이지다임마.
+</h1>
